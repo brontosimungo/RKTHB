e08951cdb983afcbffecc52b4d66ba4f6b7b6c73
brontosimungo <brontosimungo@outlook.co.id>
2025-05-26 01:30:10 +0700

    Update jadwal.js

--- data/jadwal.js
+++ data/jadwal.js
@@ -4,62 +4,62 @@ const jadwalKA = {
       "stasiun": "Parungpanjang",
       "kode": "PRP",
       "waktuTiba": null,
-      "waktuBerangkat": "04:37:00",
+      "waktuBerangkat": "1:30:00",
       "koordinat": [106.56986717482634, -6.344067067854271]
     },
     {
       "stasiun": "Cilejit",
       "kode": "CJT",
-      "waktuTiba": "04:46:00",
-      "waktuBerangkat": "04:47:00",
+      "waktuTiba": "1:39:00",
+      "waktuBerangkat": "1:40:00",
       "koordinat": [106.50957548522325, -6.354338088636527]
     },
     {
       "stasiun": "Daru",
       "kode": "DAR",
-      "waktuTiba": "04:48:00",
-      "waktuBerangkat": "04:49:00",
+      "waktuTiba": "1:41:00",
+      "waktuBerangkat": "1:42:00",
       "koordinat": [106.49242845471764, -6.3380073481707235]
     },
     {
       "stasiun": "Tenjo",
       "kode": "TEJ",
-      "waktuTiba": "04:55:00",
-      "waktuBerangkat": "04:56:00",
+      "waktuTiba": "1:48:00",
+      "waktuBerangkat": "1:49:00",
       "koordinat": [106.46165623778293, -6.327284987533221]
     },
     {
       "stasiun": "Tigaraksa",
       "kode": "TGS",
-      "waktuTiba": "04:57:00",
-      "waktuBerangkat": "04:58:00",
+      "waktuTiba": "1:51:00",
+      "waktuBerangkat": "1:52:00",
       "koordinat": [106.43419786661822, -6.328651297719585]
     },
     {
       "stasiun": "Cikoya",
       "kode": "CKY",
-      "waktuTiba": "05:00:00",
-      "waktuBerangkat": "05:01:00",
+      "waktuTiba": "1:54:00",
+      "waktuBerangkat": "01:55:00",
       "koordinat": [106.41178218756794, -6.335556384632846]
     },
     {
       "stasiun": "Maja",
       "kode": "MJ",
-      "waktuTiba": "05:04:00",
-      "waktuBerangkat": "05:05:00",
+      "waktuTiba": "1:58:00",
+      "waktuBerangkat": "1:59:00",
       "koordinat": [106.39653385009929, -6.332319947755879]
     },
     {
       "stasiun": "Citeras",
       "kode": "CTR",
-      "waktuTiba": "05:13:00",
-      "waktuBerangkat": "05:14:00",
+      "waktuTiba": "2:07:00",
+      "waktuBerangkat": "2:08:00",
       "koordinat": [106.3326800666184, -6.33475860707869]
     },
     {
       "stasiun": "Rangkasbitung",
       "kode": "RK",
-      "waktuTiba": "05:25:00",
+      "waktuTiba": "2:19:00",
       "waktuBerangkat": null,
       "koordinat": [106.25153216983001, -6.352653602595517]
     }
