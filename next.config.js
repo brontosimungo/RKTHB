60a14eb38a95f063f521df21181ce78c0993af40
brontosimungo <brontosimungo@outlook.co.id>
2025-05-25 18:54:50 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.json$/,
-      loader: 'json5-loader',
-      type: 'javascript/auto'
-    });
-    return config;
-  }
 };
 
 module.exports = nextConfig;
