445f80c053d6766cc1224b6b9a90aa9ea5c186cd
brontosimungo <brontosimungo@outlook.co.id>
2025-05-25 19:03:18 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 // pages/_app.js
-import '@/styles/globals.css'; // ✅ Impor global CSS di sini
+import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
