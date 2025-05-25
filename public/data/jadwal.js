3cf4378151e1ddf3327877deabc29c755be02ec2
brontosimungo <brontosimungo@outlook.co.id>
2025-05-26 00:52:00 +0700

    Create jadwal.js

---
+++ public/data/jadwal.js
@@ -0,0 +1,70 @@
+const jadwalKA = {
+  KA1606: [
+    {
+      "stasiun": "Parungpanjang",
+      "kode": "PRP",
+      "waktuTiba": null,
+      "waktuBerangkat": "04:37:00",
+      "koordinat": [106.56986717482634, -6.344067067854271]
+    },
+    {
+      "stasiun": "Cilejit",
+      "kode": "CJT",
+      "waktuTiba": "04:46:00",
+      "waktuBerangkat": "04:47:00",
+      "koordinat": [106.50957548522325, -6.354338088636527]
+    },
+    {
+      "stasiun": "Daru",
+      "kode": "DAR",
+      "waktuTiba": "04:48:00",
+      "waktuBerangkat": "04:49:00",
+      "koordinat": [106.49242845471764, -6.3380073481707235]
+    },
+    {
+      "stasiun": "Tenjo",
+      "kode": "TEJ",
+      "waktuTiba": "04:55:00",
+      "waktuBerangkat": "04:56:00",
+      "koordinat": [106.46165623778293, -6.327284987533221]
+    },
+    {
+      "stasiun": "Tigaraksa",
+      "kode": "TGS",
+      "waktuTiba": "04:57:00",
+      "waktuBerangkat": "04:58:00",
+      "koordinat": [106.43419786661822, -6.328651297719585]
+    },
+    {
+      "stasiun": "Cikoya",
+      "kode": "CKY",
+      "waktuTiba": "05:00:00",
+      "waktuBerangkat": "05:01:00",
+      "koordinat": [106.41178218756794, -6.335556384632846]
+    },
+    {
+      "stasiun": "Maja",
+      "kode": "MJ",
+      "waktuTiba": "05:04:00",
+      "waktuBerangkat": "05:05:00",
+      "koordinat": [106.39653385009929, -6.332319947755879]
+    },
+    {
+      "stasiun": "Citeras",
+      "kode": "CTR",
+      "waktuTiba": "05:13:00",
+      "waktuBerangkat": "05:14:00",
+      "koordinat": [106.3326800666184, -6.33475860707869]
+    },
+    {
+      "stasiun": "Rangkasbitung",
+      "kode": "RK",
+      "waktuTiba": "05:25:00",
+      "waktuBerangkat": null,
+      "koordinat": [106.25153216983001, -6.352653602595517]
+    }
+  ],
+  // dst, tambah KA003, KA004, ...
+};
+
+export default jadwalKA;
