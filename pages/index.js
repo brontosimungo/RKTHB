2a492470f00f7fdc411773d1b9c2e61a5723faa0
brontosimungo <brontosimungo@outlook.co.id>
2025-05-26 01:10:31 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Map from '../components/Map';
 import Ka1672Schedule from '../components/Ka1672Schedule';
-import jadwalKA from '../public/data/jadwal.json';
+import jadwalKA from '../public/data/jadwal';
 import { timeStringToMinutes, interpolatePosition } from '../utils/interpolateTrainPosition';
 
 export default function Home() {
