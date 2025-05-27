db7a554f449338dbeb4f906c8461a0acba66e593
brontosimungo <brontosimungo@outlook.co.id>
2025-05-27 14:04:35 +0700

    Update jadwal.js

--- data/jadwal.js
+++ data/jadwal.js
@@ -1,70 +1,139 @@
 const jadwalKA = {
   KA1606: [
     {
-      "stasiun": "Parungpanjang",
-      "kode": "PRP",
+      "stasiun": "Rangkasbitung",
+      "kode": "RK",
       "waktuTiba": null,
-      "waktuBerangkat": "1:30:00",
-      "koordinat": [106.56986717482634, -6.344067067854271]
-    },
-    {
-      "stasiun": "Cilejit",
-      "kode": "CJT",
-      "waktuTiba": "1:39:00",
-      "waktuBerangkat": "1:40:00",
-      "koordinat": [106.50957548522325, -6.354338088636527]
-    },
-    {
-      "stasiun": "Daru",
-      "kode": "DAR",
-      "waktuTiba": "1:41:00",
-      "waktuBerangkat": "1:42:00",
-      "koordinat": [106.49242845471764, -6.3380073481707235]
-    },
-    {
-      "stasiun": "Tenjo",
-      "kode": "TEJ",
-      "waktuTiba": "1:48:00",
-      "waktuBerangkat": "1:49:00",
-      "koordinat": [106.46165623778293, -6.327284987533221]
-    },
-    {
-      "stasiun": "Tigaraksa",
-      "kode": "TGS",
-      "waktuTiba": "1:51:00",
-      "waktuBerangkat": "1:52:00",
-      "koordinat": [106.43419786661822, -6.328651297719585]
-    },
-    {
-      "stasiun": "Cikoya",
-      "kode": "CKY",
-      "waktuTiba": "1:54:00",
-      "waktuBerangkat": "01:55:00",
-      "koordinat": [106.41178218756794, -6.335556384632846]
-    },
-    {
-      "stasiun": "Maja",
-      "kode": "MJ",
-      "waktuTiba": "1:58:00",
-      "waktuBerangkat": "1:59:00",
-      "koordinat": [106.39653385009929, -6.332319947755879]
+      "waktuBerangkat": "14:30:00",
+      "koordinat": [106.25153216983001, -6.352653602595517]
     },
     {
       "stasiun": "Citeras",
       "kode": "CTR",
-      "waktuTiba": "2:07:00",
-      "waktuBerangkat": "2:08:00",
+      "waktuTiba": "14:41:00",
+      "waktuBerangkat": "14:42:00",
       "koordinat": [106.3326800666184, -6.33475860707869]
     },
     {
-      "stasiun": "Rangkasbitung",
-      "kode": "RK",
-      "waktuTiba": "2:19:00",
+      "stasiun": "Maja",
+      "kode": "MJ",
+      "waktuTiba": "14:50:00",
+      "waktuBerangkat": "14:51:00",
+      "koordinat": [106.39653385009929, -6.332319947755879]
+    },
+    {
+      "stasiun": "Cikoya",
+      "kode": "CKY",
+      "waktuTiba": "14:53:00",
+      "waktuBerangkat": "14:54:00",
+      "koordinat": [106.41178218756794, -6.335556384632846]
+    },
+    {
+      "stasiun": "Tigaraksa",
+      "kode": "TGS",
+      "waktuTiba": "14:57:00",
+      "waktuBerangkat": "14:58:00",
+      "koordinat": [106.43419786661822, -6.328651297719585]
+    },
+    {
+      "stasiun": "Tenjo",
+      "kode": "TEJ",
+      "waktuTiba": "15:01:00",
+      "waktuBerangkat": "15:02:00",
+      "koordinat": [106.46165623778293, -6.327284987533221]
+    },
+    {
+      "stasiun": "Daru",
+      "kode": "DAR",
+      "waktuTiba": "15:06:00",
+      "waktuBerangkat": "15:07:00",
+      "koordinat": [106.49242845471764, -6.3380073481707235]
+    },
+    {
+      "stasiun": "Cilejit",
+      "kode": "CJT",
+      "waktuTiba": "15:10:00",
+      "waktuBerangkat": "15:01:00",
+      "koordinat": [106.50957548522325, -6.354338088636527]
+    },
+    {
+      "stasiun": "Parungpanjang",
+      "kode": "PRP",
+      "waktuTiba": "15:18:00",
+      "waktuBerangkat": "15:20:00",
+      "koordinat": [106.56986717482634, -6.344067067854271]
+    },
+    {
+      "stasiun": "Cicayur",
+      "kode": "CC",
+      "waktuTiba": "15:26:00",
+      "waktuBerangkat": "15:27:00",
+      "koordinat": [106.6189240639803, -6.329516056022527]
+    },
+    {
+      "stasiun": "Cisauk",
+      "kode": "CSK",
+      "waktuTiba": "15:29:00",
+      "waktuBerangkat": "15:30:00",
+      "koordinat": [106.64165052031788, -6.324421929217029]
+    },
+    {
+      "stasiun": "Serpong",
+      "kode": "SRP",
+      "waktuTiba": "15:33:00",
+      "waktuBerangkat": "15:34:00",
+      "koordinat": [106.6655547063112, -6.320036867931609]
+    },
+    {
+      "stasiun": "Rawabuntu",
+      "kode": "RU",
+      "waktuTiba": "15:37:00",
+      "waktuBerangkat": "15:38:00",
+      "koordinat": [106.67630098195961, -6.314822537604881]
+    },
+    {
+      "stasiun": "Sudimara",
+      "kode": "SDM",
+      "waktuTiba": "15:42:00",
+      "waktuBerangkat": "15:43:00",
+      "koordinat": [106.71279105312398, -6.296742183297029]
+    },
+    {
+      "stasiun": "Jurangmangu",
+      "kode": "JMU",
+      "waktuTiba": "15:45:00",
+      "waktuBerangkat": "15:46:00",
+      "koordinat": [106.72905772244094, -6.288608601000101]
+    },
+    {
+      "stasiun": "Pondokranji",
+      "kode": "PDJ",
+      "waktuTiba": "15:48:00",
+      "waktuBerangkat": "15:49:00",
+      "koordinat": [106.7445653224408, -6.276460025826499]
+    },
+    {
+      "stasiun": "Kebayoran",
+      "kode": "KBY",
+      "waktuTiba": "15:56:00",
+      "waktuBerangkat": "15:57:00",
+      "koordinat": [106.78266617351764, -6.2372931635379025]
+    },
+    {
+      "stasiun": "Palmerah",
+      "kode": "PLM",
+      "waktuTiba": "16:02:00",
+      "waktuBerangkat": "16:03:00",
+      "koordinat": [106.79738242619676, -6.207572646630825]
+    },
+    {
+      "stasiun": "Tanahabang",
+      "kode": "THB",
+      "waktuTiba": "16:08:00",
       "waktuBerangkat": null,
-      "koordinat": [106.25153216983001, -6.352653602595517]
+      "koordinat": [106.81085809102169, -6.185667527250411]
     }
-  ],
-  // dst, tambah KA003, KA004, ...
+  ]
 };
 
 export default jadwalKA;
